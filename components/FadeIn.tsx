81d7c2feb04f6460a17546da7851c01116b784e6
amritsolutions <alimazhar.rezayee031@gmail.com>
2026-07-06 00:39:37 +0200

    Scroll animaties toegevoegd

--- components/FadeIn.tsx
+++ components/FadeIn.tsx
@@ -10,10 +10,10 @@ type FadeInProps = {
 export default function FadeIn({ children }: FadeInProps) {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 40 }}
+      initial={{ opacity: 0, y: 80 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.2 }}
-      transition={{ duration: 0.6, ease: "easeOut" }}
+      viewport={{ once: false, amount: 0.15 }}
+      transition={{ duration: 0.9, ease: "easeOut" }}
     >
       {children}
     </motion.div>
