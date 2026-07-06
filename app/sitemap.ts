21a4f2d2d042e8bd39dc5d38a97e3eebcd2bf577
amritsolutions <alimazhar.rezayee031@gmail.com>
2026-07-06 20:15:09 +0200

    Computerreparatie toegevoegd aan sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,6 +9,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: "https://www.amritsolutions.nl/computerreparatie",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
       url: "https://www.amritsolutions.nl/privacy",
       lastModified: new Date(),
       changeFrequency: "yearly",
