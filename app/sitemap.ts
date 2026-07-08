9a43a86830c188160e65992fe39c5d6bf99ccd92
amritsolutions <alimazhar.rezayee031@gmail.com>
2026-07-08 15:51:37 +0200

    footer verbeterd

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,19 +12,25 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://www.amritsolutions.nl/computerreparatie",
       lastModified: new Date(),
       changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://www.amritsolutions.nl/website-laten-maken",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: "https://www.amritsolutions.nl/privacy",
       lastModified: new Date(),
       changeFrequency: "yearly",
-      priority: 0.5,
+      priority: 0.4,
     },
     {
       url: "https://www.amritsolutions.nl/cookies",
       lastModified: new Date(),
       changeFrequency: "yearly",
-      priority: 0.5,
+      priority: 0.4,
     },
   ];
 }
