97b7996262b95bf441633704b4cfc1dd9bfb1ed4
amritsolutions <alimazhar.rezayee031@gmail.com>
2026-07-18 00:31:14 +0200

    Update footer contact details

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import CookieBanner from "@/components/CookieBanner";
 import Footer from "@/components/Footer";
+import WhatsAppButton from "@/components/WhatsAppButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -68,6 +69,8 @@ export default function RootLayout({
   <main className="flex-1">
     {children}
   </main>
+
+  <WhatsAppButton />
 
   <Footer />
 
